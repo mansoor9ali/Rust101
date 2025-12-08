d7b22d221e7327b88e4c04820c771c3fdbe6332b
Mansoor <mansoor.ali@live.com>
2025-12-08 01:56:12 -0500

    added database101

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+pub mod models;
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
